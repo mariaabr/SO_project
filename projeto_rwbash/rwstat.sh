72e717c0f2d8efcb1deded7f303dccc82b0bd8a9
mariaabr <maria.abrunhosa@ua.pt>
2022-12-01 18:55:41 +0000

    new commit

--- projeto_rwbash/rwstat.sh
+++ projeto_rwbash/rwstat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Maria Rafaela Alves Abrunhosa 107658 (50%)
 # Matilde Moital Portugal Sampaio Teixeira 108193 (50%)
 
@@ -7,15 +8,12 @@
 # declarations
 
 declare -A arrproc=()
-declare -A arrcomms=()
-declare -A arrusers=()
 declare -A arrread=()
 declare -A arrwrite=()
 declare -A arropt=()
 
 sort_reverse=""
-i=0
-pid=$(ps -ef | grep 'p' | awk '{print $2}') # ps command is used to list the currently running processes and their PIDs
+#pid=$(ps -ef | grep 'p' | awk '{print $2}') # ps command is used to list the currently running processes and their PIDs
 # along with some other information depends on different options
 # -ef restricts the processes running on the system in the standart format
 re='^[0-9]+([.][0-9]+)?$'
@@ -27,12 +25,13 @@ timepattern="[A-Za-z]{3} ([0-2][1-9]|[3][0-1]) ([0-1][0-9]|[2][0-3]):[0-5][0-9]"
 #     (...)
 # fi
 
-if [[ -n $pid ]]; then # -n evaluates the expressions in bash -> tests the string
-# next to it and evaluates it as "True" if string is not empty
-    echo ""
-else
-    echo "ERROR: Cannot find any"
-fi
+# if [[ -n $pid ]]; then # -n evaluates the expressions in bash -> tests the string
+# # next to it and evaluates it as "True" if string is not empty
+#     echo ""
+# else
+#     echo "ERROR: Cannot find any"
+#     exit 1
+# fi
 
 
 
@@ -52,17 +51,13 @@ else
     exit 1
 fi
 
-# printf "%d -> o valor dos segundos\n" "$segundos"
-
-
-
 while getopts ":c:s:e:u:m:M:p:rw" options; do
 
-    if [[ -z "$OPTARG" ]]; then
-        arropt[$options]="null"
-    else
-        arropt[$options]=${OPTARG} # $OPTARG gets the value of the argument "linked" to the option
-    fi
+        if [[ -z "$OPTARG" ]]; then
+            arropt[$options]="null"
+        else
+            arropt[$options]=${OPTARG} # $OPTARG gets the value of the argument "linked" to the option
+        fi
 
     case "$options" in
         c) # seleção dos processos a visualizar através de uma expressão regular
@@ -82,6 +77,7 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             start=$(date -d "$datemin" +%s);
             ;;
         e) # especificação do período temporal - data máxima
+            printf "espinafres com arroz\n"
             datemax=${arropt['e']}
             if [[ $datemax == 'null' || ${datemax:0:1} == "-" || ! "${datemax}" =~ $timepattern ]]; then
                 echo "o argumento de '-e' é 'null' ou inválido"
@@ -91,7 +87,6 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             ;;
         u) # seleção realizada pelo nome de utilizador
             user=${arropt['c']}
-            echo "estou no user!"
             if [[ $user == 'null' || ${user:0:1} == "-" || ${user} =~ $re ]]; then
                 echo "o argumento de '-u' é 'null' ou inválido"
                 exit 1
@@ -106,7 +101,10 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             ;;
         M) # gama de pids - gama máxima
             gamamax=${arropt['M']}
-            if ! [[ $gamamax != 'null' || ${gamamax:0:1} != "-" || $gamamax =~ $re || $gamamax -ge 0 ]]; then
+            if ! [[ $gamamin != 'null' || ${gamamin:0:1} != "-" || $gamamin =~ $re || $gamamin -ge 0 ]]; then
+                if [$gamamax -le $gamamin]; then
+                    echo "gama máxima menor que gama mínima"
+                fi
                 echo "o argumento de '-m' é 'null', inválido ou menor que 0"
                 exit 1
             fi
@@ -132,57 +130,85 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             ;;
     esac
 done
+
+
 shift $((OPTIND - 1))
-
-
 
 # procurar e listar os processos
 
 cd /proc
 for process in $(ls | grep -E '^[0-9]+$'); do
     if [[ -r $process/status && -r $process/io ]]; then
-        arrproc[i]=$process
-        printf "%d -> o valor do proc (pid) \n" "$process"
-
-        cd /proc/$process
-        comm=$(cat comm)
-        arrcomms[i]=$comm
-        printf "%s -> o valor do comm \n" "$comm"
-
-        userproc=$(ls -ld | awk '{print $3}')
-        arrusers[i]=$userproc
-        printf "%s -> o valor do user \n" "$userproc"
-
-        rchar=$(cat /proc/$process/io | grep rchar | grep -o -E '[0-9]+') # "|" pipe chains commands together -> takes the output from one command and feeds it to the next as input
-        wchar=$(cat /proc/$process/io | grep wchar | grep -o -E '[0-9]+')
+        pid=$(cat $process/status | grep -w Pid | tr -dc '0-9' )
+        #printf "%d -> o valor do proc (pid) \n" "$pid"
+                    
+        rchar=$(cat $process/io | grep rchar | grep -o -E '[0-9]+') # "|" pipe chains commands together -> takes the output from one command and feeds it to the next as input
+        wchar=$(cat $process/io | grep wchar | grep -o -E '[0-9]+')
                 
-        # echo "estás aqui, patinho?"
-        printf "%d -> o valor do rchar \n" "$rchar"
-        printf "%d -> o valor do wchar \n" "$wchar"
+        #printf "%d -> o valor do rchar \n" "$rchar"
+        #printf "%d -> o valor do wchar \n" "$wchar"
 
-        arrread[i]=$rchar
-        arrwrite[i]=$wchar
-
-        i=$((i+1))
+        if [[ $rchar == 0 && $wchar == 0 ]]; then
+            continue
+        else
+            arrread[$pid]=$rchar
+            arrwrite[$pid]=$wchar
+        fi
     fi
+    
 done
 cd
 
+sleep $segundos
+
+# segunda leitura
+
+cd /proc
+for process in $(ls | grep -E '^[0-9]+$'); do
+    if [[ -r $process/status && -r $process/io ]]; then
+        pid=$(cat $process/status | grep -w Pid | tr -dc '0-9' )
+        #printf "%d -> o valor do proc (pid) 2\n" "$pid"
+        comm=$(cat $process/comm | tr " " "_") # ir buscar o comm,e retirar os espaços e substituir por '_' nos comm's com 2 nomes
+        #printf "%s -> o valor do comm 2\n" "$comm"
+        userproc=$(ps -o user= -p $pid)
+        #printf "%s -> o valor do user 2\n" "$userproc"
+
+        # FILTRAR PROCESSOS COM CONDIÇÕES AQUI :))
+
+        # EHEHEH
+
+        # COISAS
+        
+        rchar_2=$(cat $process/io | grep rchar | grep -o -E '[0-9]+')
+        wchar_2=$(cat $process/io | grep wchar | grep -o -E '[0-9]+')
+
+        subread=$(($rchar_2 - ${arrread[$pid]}))
+        #echo "calculo $subreas"
+        #printf "eu vou comer\n"
+        subwrite=$(($wchar_2 - ${arrwrite[$pid]}))
+        #echo "calculo 2 $subwrite"
 
 
-
-# sleep $segundos
-
-
-
-# PRINT da tabela
-for i in $arrproc[@]; do
-    echo "cucumber"
-    printf "%-15s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER" "RATEW" "DATE"
-    printf "%-15s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "${arrcomms[i]}" "${arrusers[i]}" "${arrproc[i]}" "${arrread[i]}" "${arrwrite[i]}" "rater" "raterw" "COISAS"
+        if [[ $subread == 0 && $subwrite == 0 ]]; then
+            continue
+        else
+            rater=$(bc <<<"scale=2; $subread/$segundos")
+            ratew=$(bc <<<"scale=2; $subwrite/$segundos")
+            rater=${rater/#./0.}
+            ratew=${ratew/#./0.}
+            # echo "calculo 3 $rater"
+            # echo "calculo 4 $ratew"
+            # printf "%s -> o valor do rater 2\n" "$rater"
+            # printf "%s -> o valor do ratew 2\n" "$ratew"
+        fi
+        arrproc[$pid]=$(printf "%-20s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "$comm" "$user" "$pid" "$subread" "$subwrite" "$rater" "$ratew" "cucu")   
+    fi
 done
 
 
+# PRINT da tabela
+printf "%-15s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER" "RATEW" "DATE"
+printf '%s \n' "${arrproc[@]}"
 
 
-# ENDS HERE
+# ENDSHERE
