62fb2c5db43fe6518108fe13e9e0424e323565ee
mariaabr <maria.abrunhosa@ua.pt>
2022-12-01 22:42:06 +0000

    7 commit

--- projeto_rwbash/rwstat.sh
+++ projeto_rwbash/rwstat.sh
@@ -53,7 +53,7 @@ fi
 
 while getopts ":c:s:e:u:m:M:p:rw" options; do
 
-        if [[ -z "$OPTARG" ]]; then
+        if [[ -z "$OPTARG" ]]; then # -z -> string is null, evaluates if the string has zero length
             arropt[$options]="null"
         else
             arropt[$options]=${OPTARG} # $OPTARG gets the value of the argument "linked" to the option
@@ -86,7 +86,7 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             end=$(date -d "$datemax" +%s);
             ;;
         u) # seleção realizada pelo nome de utilizador
-            user=${arropt['c']}
+            user=${arropt['u']}
             if [[ $user == 'null' || ${user:0:1} == "-" || ${user} =~ $re ]]; then
                 echo "o argumento de '-u' é 'null' ou inválido"
                 exit 1
@@ -111,9 +111,8 @@ while getopts ":c:s:e:u:m:M:p:rw" options; do
             ;;
         p) # numero de processos a visualizar
             numproc=${arropt['p']}
-            echo "estou no p!"
             if ! [[ $numproc =~ $re ]]; then
-                echo "o argumento de '-p' é 'null' tem de ser um número"
+                echo "o argumento de '-p' tem de ser um número"
                 exit 1
             fi
             ;;
@@ -171,11 +170,20 @@ for process in $(ls | grep -E '^[0-9]+$'); do
         comm=$(cat $process/comm | tr " " "_") # ir buscar o comm,e retirar os espaços e substituir por '_' nos comm's com 2 nomes
         #printf "%s -> o valor do comm 2\n" "$comm"
         userproc=$(ps -o user= -p $pid)
-        #printf "%s -> o valor do user 2\n" "$userproc"
-
+        # printf "%s -> o valor do user \n" "$userproc"
+        data=$(LC_ALL=EN_us.utf8 ls -ld /proc/$process | awk '{print $6 " " $7 " " $8}')
         # FILTRAR PROCESSOS COM CONDIÇÕES AQUI :))
 
+        if [[ -v arropt[u] && ! ${arropt['u']} == $userproc ]]; then # -v tells the shell to run in verbose mode -> is useful
+        # in locating the line of the script that as created an error
+            continue # se não existir o user escolhido não adiciona à lista
+        fi
+
         # EHEHEH
+
+        if [[ -v arropt[c] && ! $comm =~ ${arropt['c']} ]]; then # escolhe quais "$comm" se adequam ao padrão inserido
+            continue
+        fi
 
         # COISAS
         
@@ -183,7 +191,7 @@ for process in $(ls | grep -E '^[0-9]+$'); do
         wchar_2=$(cat $process/io | grep wchar | grep -o -E '[0-9]+')
 
         subread=$(($rchar_2 - ${arrread[$pid]}))
-        #echo "calculo $subreas"
+        #echo "calculo $subread"
         #printf "eu vou comer\n"
         subwrite=$(($wchar_2 - ${arrwrite[$pid]}))
         #echo "calculo 2 $subwrite"
@@ -201,14 +209,22 @@ for process in $(ls | grep -E '^[0-9]+$'); do
             # printf "%s -> o valor do rater 2\n" "$rater"
             # printf "%s -> o valor do ratew 2\n" "$ratew"
         fi
-        arrproc[$pid]=$(printf "%-20s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "$comm" "$user" "$pid" "$subread" "$subwrite" "$rater" "$ratew" "cucu")   
+
+        arrproc[$pid]=$(printf "%-20s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "$comm" "$userproc" "$pid" "$subread" "$subwrite" "$rater" "$ratew" "$data")   
     fi
 done
 
 
 # PRINT da tabela
-printf "%-15s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER" "RATEW" "DATE"
-printf '%s \n' "${arrproc[@]}"
 
+if ! [[ -v arropt[p] ]]; then
+    p=${#arrproc[@]} # por default o valor de p é igual ao tamanho do array de todos os processos
+    # # -> indica o tamanho do array
+else
+    p=${arropt['p']} # numero de processos inserido
+fi
+
+printf "%-15s %-13s %8s %11s %11s %11s %11s %13s %13s %13s\n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER" "RATEW" "DATE"
+printf '%s \n' "${arrproc[@]}" | head -n $p
 
 # ENDSHERE
